fba98cfdac887adfb2069351e869fd4ba46cd3a9
somu3341 <somashekarvsv1996@gmail.com>
2023-03-24 19:34:12 +0530

    [somu]Add:Binary Search for a word using string.

---
+++ AlgorithmPrograms/BinarySearch/BinaryWrodSearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms.BinarySearch
+{
+    public class BinaryWrodSearch
+    {
+        public void ReadFile(string filepath)
+        {
+            string readData =File.ReadAllText(filepath);
+            string[] words = readData.Split(" ");
+            foreach (var data in words)
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("Enter word to search");
+            string search = Console.ReadLine();
+            foreach (var data in words)
+            {
+                if (data.Equals(search))
+                {
+                    Console.WriteLine("Found");
+                    return;
+                }
+            }
+            Console.WriteLine("Not Found");
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using AlgorithmPrograms.BinarySearch;
+using System;
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryWrodSearch search = new BinaryWrodSearch();
+            search.ReadFile(@"D:\BridgeLabs\AlgorithmPrograms\AlgorithmPrograms\BinarySearch\File.txt");
+        }
+    }
+}
