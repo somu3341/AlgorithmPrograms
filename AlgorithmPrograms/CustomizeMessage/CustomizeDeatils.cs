16ead70bb270ed178d8f07b7dfe51c78d242e673
somu3341 <somashekarvsv1996@gmail.com>
2023-03-25 19:57:13 +0530

    [somu]Add:Printing Customized message deatils.

--- AlgorithmPrograms/CustomizeMessage/CustomizeDeatils.cs
+++ AlgorithmPrograms/CustomizeMessage/CustomizeDeatils.cs
@@ -10,10 +10,11 @@ namespace AlgorithmPrograms.CustomizeMessage
     {
         public void Customize()
         {
-            string line = "Hello <<name>>, We have your full name as <<full name>> in our system. your contact number is 91-xxxxxxxxx";
+            string line = "Hello <<name>>, We have your full name as <<full name>> in our system. your contact number is 91-xxxxxxxxx. Today's Date <<date>>.";
             line = line.Replace("<<name>>","Somu");
             line = line.Replace("<<full name>>","SomaShekar");
             line = line.Replace("91-xxxxxxxxx","91-987654321");
+            line = line.Replace("<<date>>","25/03/2023");
             Console.WriteLine(line);
         }
         
